8a73be3fe41adc3c8ec5110bab191e83ffefcf46
reenuchahal <reenuchahal@gmail.com>
2013-09-11 12:43:05 -0400

    style.css and index.php modifications

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css" />
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="shortcut icon" href="img/favicon.ico">
+</head>
+<body>
+<header>
+</header>
+<footer>
+</footer>
+
+<!-- Scripts -->
+<script src="//code.jquery.com/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Scripts Ends -->
+</body>
+</html>
